6b4abf87a8e85bda13650c5226fb21cdca171a8c
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-15 20:08:36 +0300

    fix: changed units images

--- src/components/board/BoardComponent.tsx
+++ src/components/board/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Board.scss'
 import { FC } from 'react'
 import { Board } from '../../models/Board'
