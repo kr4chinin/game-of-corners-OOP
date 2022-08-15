f097970f6b183a66025699d877256999e7f9495e
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-15 19:03:04 +0300

    fix: getCurrentTime output format

--- src/helpers/getCurrentTime.ts
+++ src/helpers/getCurrentTime.ts
@@ -1,4 +1,13 @@
 export function getCurrentTime() {
-    const now = new Date()
-    return now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+	const now = new Date()
+
+	let hours = String(now.getHours())
+	let minutes = String(now.getMinutes())
+	let seconds = String(now.getSeconds())
+
+	hours.length === 1 && (hours = '0' + hours)
+	minutes.length === 1 && (minutes = '0' + minutes)
+	seconds.length === 1 && (seconds = '0' + seconds)
+
+	return `${hours}:${minutes}:${seconds}`
 }
