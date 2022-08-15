095f23cd2455546f8dd4ff72fd17ed7292f80141
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-15 14:42:44 +0300

    feat: colors enum, init squares functionality

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,7 +1,20 @@
+import { Colors } from "./Colors";
 import { Square } from "./Square";
 
 export class Board {
     squares: Square[][] = []
 
-
+    public initSquares() {
+        for (let i = 0; i < 8; i++) {
+			const row: Square[] = []
+			for (let j = 0; j < 8; j++) {
+				if ((i + j) % 2 !== 0) {
+					row.push(new Square(this, j, i, Colors.BLACK))
+				} else {
+					row.push(new Square(this, j, i, Colors.WHITE))
+				}
+			}
+			this.squares.push(row)
+		}
+    }
 }
--- src/models/Square.ts
+++ src/models/Square.ts
@@ -1,3 +1,26 @@
+import { Board } from "./Board"
+import { Colors } from "./Colors"
+import uniqid from 'uniqid'
+
 export class Square {
-    
+    readonly x: number
+	readonly y: number
+	readonly color: Colors
+	board: Board
+	available: boolean
+	id: string
+
+    constructor(
+		board: Board,
+		x: number,
+		y: number,
+		color: Colors,
+	) {
+		this.board = board
+		this.x = x
+		this.y = y
+		this.color = color
+		this.available = false
+		this.id = uniqid()
+	}
 }
