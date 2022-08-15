9be202931edc4be10c7500e0d6095887c3983b91
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-15 16:06:00 +0300

    fix: moved player instance out of state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import BoardComponent from './components/board/BoardComponent'
 import { Board } from './models/Board'
 import { Colors } from './models/Colors'
@@ -6,14 +6,16 @@ import { Player } from './models/Player'
 
 const App = () => {
 	const [board, setBoard] = useState(new Board())
-    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
-	const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
-	const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+
+    const whitePlayer = useMemo(() => new Player(Colors.WHITE), [])
+	const blackPlayer = useMemo(() => new Player(Colors.BLACK), [])
+
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
 
     useEffect(() => {
         start()
         setCurrentPlayer(whitePlayer)
-    }, [])
+    }, [whitePlayer])
 
 	function start() {
 		const newBoard = new Board()
