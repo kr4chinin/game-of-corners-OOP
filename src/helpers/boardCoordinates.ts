cde35fa7a2c5592c727514f82e0f567e7b22692b
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-15 19:55:49 +0300

    refactor: moved digits and letters arrs to helpers

--- src/helpers/boardCoordinates.ts
+++ src/helpers/boardCoordinates.ts
@@ -1,7 +1,9 @@
 const digits = [1, 2, 3, 4, 5, 6, 7, 8]
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-export default {
-    digits,
-    letters
+const boardCoordinates = {
+	digits,
+	letters
 }
+
+export default boardCoordinates
