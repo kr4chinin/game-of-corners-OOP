0c0277f536ba410f5c4ebf4e176e869253eece62
Ilya Kruchinin <kr4chinin@mail.ru>
2022-08-17 10:25:42 +0300

    feat: more unit moves varieties

--- src/models/Unit.ts
+++ src/models/Unit.ts
@@ -17,28 +17,158 @@ export class Unit extends Figure {
 		const dx = target.x - this.square.x
 		const dy = target.y - this.square.y
 
-		// move vertically
-		if ((dx === 1 || dx === -1) && dy === 0) return true
+		// vertical movement
+		if (dy === 0) {
+			switch (dx) {
+				// move left / right
+				case 1:
+					return true
+				case -1:
+					return true
+				// jump over 1 unit
+				case 2:
+					if (
+						this.square.board.getSquare(this.square.x + 1, this.square.y).figure
+					)
+						return true
+					break
+				case -2:
+					if (
+						this.square.board.getSquare(this.square.x - 1, this.square.y).figure
+					)
+						return true
+					break
+				// jump over 2 units
+				case 4:
+					if (
+						this.square.board.getSquare(this.square.x + 1, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x + 2, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x + 3, this.square.y).figure
+					)
+						return true
+					break
+				case -4:
+					if (
+						this.square.board.getSquare(this.square.x - 1, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x - 2, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x - 3, this.square.y).figure
+					)
+						return true
+					break
+				// jump over 3 units
+				case 6:
+					if (
+						this.square.board.getSquare(this.square.x + 1, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x + 2, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x + 3, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x + 4, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x + 5, this.square.y).figure
+					)
+						return true
+					break
+				case -6:
+					if (
+						this.square.board.getSquare(this.square.x - 1, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x - 2, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x - 3, this.square.y)
+							.figure &&
+						!this.square.board.getSquare(this.square.x - 4, this.square.y)
+							.figure &&
+						this.square.board.getSquare(this.square.x - 5, this.square.y).figure
+					)
+						return true
+					break
+				default:
+					break
+			}
+		}
 
-		// move horizontally
-		if ((dy === 1 || dy === -1) && dx === 0) return true
-
-		// jump over one vertically
-		if (
-			(dx === 2 || dx === -2) &&
-			dy === 0 &&
-			this.square.board.getSquare(this.square.x + dx / 2, this.square.y).figure
-		)
-			return true
-
-		// jump over one horizontally
-		if (
-			(dy === 2 || dy === -2) &&
-			dx === 0 &&
-			this.square.board.getSquare(this.square.x, this.square.y + dy / 2).figure
-		)
-			return true
-
-		return false
+		// horizontal movement
+		if (dx === 0) {
+			switch (dy) {
+				// move forward / backwards
+				case 1:
+					return true
+				case -1:
+					return true
+				// jump over 1 unit
+				case 2:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y + 1).figure
+					)
+						return true
+					break
+				case -2:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y - 1).figure
+					)
+						return true
+					break
+				// jump over 2 units
+				case 4:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y + 1)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y + 2)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y + 3).figure
+					)
+						return true
+					break
+				case -4:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y - 1)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y - 2)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y - 3).figure
+					)
+						return true
+					break
+				// jump over 3 units
+				case 6:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y + 1)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y + 2)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y + 3)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y + 4)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y + 5).figure
+					)
+						return true
+					break
+				case -6:
+					if (
+						this.square.board.getSquare(this.square.x, this.square.y - 1)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y - 2)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y - 3)
+							.figure &&
+						!this.square.board.getSquare(this.square.x, this.square.y - 4)
+							.figure &&
+						this.square.board.getSquare(this.square.x, this.square.y - 5).figure
+					)
+						return true
+					break
+				default:
+					break
+			}
+		}
+        
+        return false
 	}
 }
